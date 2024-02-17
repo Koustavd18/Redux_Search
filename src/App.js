9853da3f6fd3fdc9516409d887b9a994edc7b8d1
Koustav Das <kdkdas8@gmail.com>
2024-02-17 14:08:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container is-fluid">
+      <CarForm />
+      <CarSearch />
+      <CarList />
+      <CarValue />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import "bulma/css/bulma.css";
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import App from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
